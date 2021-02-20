3e0d4e73dbc97bb8bebae96dfc38f2836485d75b
DohnalMichal <michal.dohnal7@gmail.com>
2021-02-20 20:50:03 +0100

    initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '846ed537e6cd4e184596fe5d395bf09b'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'e982ffe4-6b7e-4855-86f6-18d3dd22d863'
+};
